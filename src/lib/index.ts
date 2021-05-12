f146734e15346b38cbe47ffe65ea16d8df18c1d6
Peter Kottas <petokottas@gmail.com>
2021-05-12 21:33:51 +0200

    Deploy commit

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,7 @@
-/*export * from './validators/index';*/
 export { default as background } from './background';
 export { default as image } from './image';
+export { default as spacer } from './spacer';
+export { default as video } from './video';
+export { default as divider } from './divider';
+export { default as slate } from './slate';
+export { default as BottomToolbar } from './common/components/bottomToolbar/BottomToolbar';
