2d3473eb9c6effd3537b9e4a8cadedd67b295e0b
Peter Kottas <petokottas@gmail.com>
2022-01-31 14:25:15 +0100

    Deploy commit

--- src/lib/common/components/slateEditor/slateEmptyValue.ts
+++ src/lib/common/components/slateEditor/slateEmptyValue.ts
@@ -1,10 +1,10 @@
 import { SlateValue } from '../../types/slate/SlateValue';
-export const slateEmptyValue: () => SlateValue = () => [
+export const slateEmptyValue: (text?: string) => SlateValue = (text = '') => [
   {
     type: 'PARAGRAPH',
     children: [
       {
-        text: '',
+        text,
       },
     ],
   },
